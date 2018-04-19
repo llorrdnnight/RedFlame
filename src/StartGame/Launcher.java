5c03606e43d718cf78ede16db2c90a5a483f2827
Levi N <levi.nauwelaerts@gmail.com>
2018-04-19 14:11:16 +0200

    2nd commit

---
+++ src/StartGame/Launcher.java
@@ -0,0 +1,13 @@
+package StartGame;
+
+import ui.MainScreen;
+
+public class Launcher {
+	public static void main(String[] args) {
+		new MainScreen();	         
+	}
+}
+
+// to launch our start screen run this file to boot up game
+
+
---
+++ test/domain/AllTests.java
@@ -0,0 +1,19 @@
+package domain;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+//these are all the tests
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	/*
+	[name].class,  
+	SpelerTest.class,  //02
+	CirkelTest.class, 
+	RechthoekTest.class,
+	DriehoekTest.class,
+	})
+	*/
+public class AllTests {
+
+}
